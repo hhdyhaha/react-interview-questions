e3f5867c580d0f63e5a10833efa7993c6ebda027
hhdyhaha <1164549585@qq.com>
2025-06-17 16:20:53 +0800

    更新生产环境配置，修改API地址为新的服务器地址，并优化index.html文件结构，添加Umami分析脚本。同时新增pnpm锁定文件以管理依赖。

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://123.56.15.95:8000',
+        target: 'http://204.152.194.234:8000',
         changeOrigin: true,
         secure:false,
         rewrite: (path) => path.replace(/^\/api/, ''),
